1f6c42481b4a5fd8dc59b888e9eddbea2d86ebd3
RicoCruzSuave <RicoCruzSuave@users.noreply.github.com>
2023-07-05 17:38:49 -0700

    MVM Event Window Add

--- Misc/Atom.gd
+++ Misc/Atom.gd
@@ -8,8 +8,14 @@ class_name Atom
 func _init():
 	pass
 
-func act():
-	pass
+#func act(neighbors : Dictionary) -> Array:
+	#Intake neighbors and return swap coordinates in pairs
+	#Chess style eg (1,2),(3,4) 
+	#	- world node will perform swap cells on these two coordinates
+	#	- can also replace second coord with a transformation (1,2), { "type" : TYPE.FIRE }
+	#	- maybe use a dict? or make an instruction struct? 
+	# 	- transformation made directly on other cell?
+	
 
 func get_color():
 	pass
